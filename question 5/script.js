6c64ca4e1592b03b1911621f43a2dcb321b7adf9
unknown <godhanidivyesh44@gmail.com>
2023-05-13 13:35:13 +0530

    Question 5 is Added

---
+++ question 5/script.js
@@ -0,0 +1,9 @@
+// 05. Declare a varibale and assign string value to it and then split it into an array using split() method
+
+// ** Start Code 
+
+let str = "This is split message";
+
+let strmessage = str.split("");
+
+console.log(strmessage);
