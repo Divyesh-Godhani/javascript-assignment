b7be2329899f4e9c0e8f20adec16a5fb52257625
unknown <godhanidivyesh44@gmail.com>
2023-05-13 17:46:21 +0530

    Add comment on question 8

--- question 8/script.js
+++ question 8/script.js
@@ -1,7 +1,7 @@
 // 08. Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
 
 
-// ** start **
+// ** start Code **
 
 let str = "  hello world!  ";
 
