ad154286f5cc110f03017db24015dd777ee250d9
unknown <godhanidivyesh44@gmail.com>
2023-05-13 13:09:03 +0530

    Question 4  is Added

---
+++ question 4/script.js
@@ -0,0 +1,11 @@
+// 04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+
+// ** start code 
+
+let str = "Javascript languages is Most Famous Programming Language"
+
+if (str.includes("script")){
+    console.log("The word 'Script' is included in the message.");
+}else {
+    console.log("The word 'Script' is not included in the message.");
+  }
