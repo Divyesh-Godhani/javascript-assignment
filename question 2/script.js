b3bff2e104b5050c242e356a32f6e0815a621cfa
unknown <godhanidivyesh44@gmail.com>
2023-05-13 12:10:35 +0530

    Question 2 is Added

---
+++ question 2/script.js
@@ -0,0 +1,11 @@
+// 02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+// ** start Code **
+
+let firstname = "Divyesh";
+let lastname  = "Godhani";
+let maritalstatus = "single";
+let country = "india";
+let age = 23;
+
+console.log(` My Name is ${firstname} ${lastname} and maritalstatus is ${maritalstatus} and live in ${country} and Age is ${age}`);
