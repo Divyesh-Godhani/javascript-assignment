165d5b21a0f9c4aacda86543573caa54006d389c
unknown <godhanidivyesh44@gmail.com>
2023-05-13 13:45:14 +0530

    Question 6 is Added

---
+++ question 6/script.js
@@ -0,0 +1,7 @@
+// 06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+let comapany = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+
+let groupofcomapany = comapany.split(", ");
+
+console.log(groupofcomapany);
