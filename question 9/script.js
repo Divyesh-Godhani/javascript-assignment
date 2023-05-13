fa23980d24550e65ca6f900522ae72486b90bb2c
unknown <godhanidivyesh44@gmail.com>
2023-05-13 18:08:24 +0530

    Add comment on question 9

---
+++ question 9/script.js
@@ -0,0 +1,21 @@
+// 09. Boolean value is either true or false.
+    // - Write three JavaScript statement example which provide truthy value.
+
+    // - Write three JavaScript statement example which provide falsy value.
+
+// ** code start **  
+
+// - Write three JavaScript statement example which provide truthy value.
+
+let x = 1+2; // x is truthy because it has a numeric value
+let y = "Hello"; // y is truthy because it has a string value
+let z = true; // z is truthy because it has a boolean value of true
+
+
+// - Write three JavaScript statement example which provide falsy value.
+
+let a = 0;  // a is falsy because it has a numeric value of 0
+
+let b = ""; // b is falsy because it has an empty string value
+
+let c = false ; // c is falsy because it has a boolean value of false
