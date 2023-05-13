1c0a7fc192d3316ef60694df0af7dfcc77f5a3eb
unknown <godhanidivyesh44@gmail.com>
2023-05-13 12:47:11 +0530

    Question 3 is Added

---
+++ question 3/script.js
@@ -0,0 +1,9 @@
+// 03. Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+// ** Start Code ** 
+
+let str = "Hello World";
+
+let strmessage = str.toUpperCase();
+
+console.log(strmessage);
