488b7513afc9863e5e25f0aabfcce183ef71f4fd
unknown <godhanidivyesh44@gmail.com>
2023-05-13 11:58:39 +0530

    question 1 Added

--- question 1/variable.js
+++ question 1/variable.js
@@ -1,2 +1,18 @@
 
 // 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+// ** code Started ** 
+
+// declare variable with different datatype
+
+let str = "Hello World"; // string
+let bool = true; // boolean
+let undef = undefined; // undefine
+let n = null ; // null
+
+// display with data type
+
+console.log(`value of str: ${str}, Data Type: ${typeof str}`);
+console.log(`value of bool: ${bool}, Data Type: ${typeof bool}`);
+console.log(`value of undef: ${undef}, Data Type: ${typeof undef}`);
+console.log(`value of n: ${n}, Data Type: ${typeof n}`);
